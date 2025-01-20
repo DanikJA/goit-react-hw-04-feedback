1fd0f654278411ac7f573ba3cadff1ee89f27b82
Даніїл <140548263+DanikJA@users.noreply.github.com>
2025-01-20 22:18:22 +0200

    Initial commit

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,10 @@
+import { StyledSection, Title } from './SectionStyled';
+
+export const Section = ({ title, children }) => {
+  return (
+    <StyledSection>
+      <Title>{title}</Title>
+      {children}
+    </StyledSection>
+  );
+};
